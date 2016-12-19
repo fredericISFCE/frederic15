73761408d557917dbaeb49b981f98a3a0b160688
fred <fred@LAPTOP-7BMEN45J>
2016-12-19 19:14:17 +0100

    deuxieme commit: nouveau H2 + paragraphe

--- index.php
+++ index.php
@@ -12,6 +12,9 @@ and open the template in the editor.
     <body>
         <h1>Projet Git ISFCE DwpWeb</h1>
         <p>Lundi 19 decembre 2016</p>
+        
+        <h2> test de gitHub</h2>
+        <p>deuxieme commit</p>
         <?php
         // put your code here
         ?>
